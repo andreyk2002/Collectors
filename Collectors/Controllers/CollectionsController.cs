71a503ed729aac89cc22a88ff94e5fc0db93991c
Andrey Kuksa <andrey20011005@gmail.com>
2021-01-07 14:34:56 +0300

    Added opportunity to edit collections

--- Collectors/Controllers/CollectionsController.cs
+++ Collectors/Controllers/CollectionsController.cs
@@ -31,9 +31,9 @@ namespace Collectors.Controllers
         }
         public IActionResult Index()
         {
-           ViewBag.Collections = User.IsInRole(UserRoles.admin.ToString()) ?
-            db.Collections.ToList() : db.Collections.Where(c =>
-                c.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier)).ToList();
+            ViewBag.Collections = User.IsInRole(UserRoles.admin.ToString()) ?
+             db.Collections.ToList() : db.Collections.Where(c =>
+                 c.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier)).ToList();
             return View();
         }
 
@@ -55,13 +55,13 @@ namespace Collectors.Controllers
 
         private Collection CreateCollectionFromModel(CollectionCreateModel model)
         {
-           return new Collection
-           {
-               ShortDescription = model.ShortDescription,
-               ThemeId = (byte)model.CollectionTheme,
-               AdditionalItemsFields = string.Join(",", model.AdditionalFields),
-               UserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
-           };
+            return new Collection
+            {
+                ShortDescription = model.ShortDescription,
+                ThemeId = (byte)model.CollectionTheme,
+                AdditionalItemsFields = string.Join(",", model.AdditionalFields),
+                UserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            };
         }
 
         public IActionResult Delete(int id)
@@ -77,40 +77,62 @@ namespace Collectors.Controllers
         {
             Collection c = db.Collections.First(c => c.Id == id);
             if (c != null)
-                return View(c);
+                return View(new CollectionEditModel
+                { CollectionId = id, ShortDescription = c.ShortDescription, OldImage = c.Image });
             return NotFound();
         }
 
-        public string ViewCollection(Collection collection)
+        [HttpPost]
+        public IActionResult Edit(CollectionEditModel collection)
         {
-            return "Пшёл нахуй я нихуя не сделал";
-        }
-
-        private int CreateMaskOfSelectedFields(IList<string> additionalFields)
-        {
-            int mask = 0;
-            for (int i = 0; i < additionalFields.Count; i++)
+            Collection c = db.Collections.First(c => c.Id == collection.CollectionId);
+            if (c != null)
             {
-                if (additionalFields[i] != null)
-                    mask += (int)Math.Pow(2, i);
+                if (collection.ShouldDeleteImage)
+                    c.Image = null;
+                else
+                    SetCollectionImageInBytes(collection.Image, c);
+                c.ShortDescription = collection.ShortDescription;
+                db.SaveChanges();
             }
-            return mask;
+                return RedirectToAction("Index");
         }
 
-        private void SetCollectionImageInBytes(IFormFile Image, Collection collection)
-        {
-            if (Image != null)
+            public string ViewCollection(Collection collection)
+            {
+                return "Пшёл нахуй я нихуя не сделал";
+            }
+
+            private int CreateMaskOfSelectedFields(IList<string> additionalFields)
+            {
+                int mask = 0;
+                for (int i = 0; i < additionalFields.Count; i++)
+                {
+                    if (additionalFields[i] != null)
+                        mask += (int)Math.Pow(2, i);
+                }
+                return mask;
+            }
+
+            private void SetCollectionImageInBytes(IFormFile Image, Collection collection)
+            {
+                if (Image != null)
+                {
+                    collection.Image = ConvertImageToBytes(Image);
+                }
+            }
+
+            private byte[] ConvertImageToBytes(IFormFile Image)
             {
                 byte[] imageData = null;
                 using (var binaryReader = new BinaryReader(Image.OpenReadStream()))
                 {
                     imageData = binaryReader.ReadBytes((int)Image.Length);
                 }
-                collection.Image = imageData;
+                return imageData;
             }
         }
     }
-}
 
 
 
---
+++ Collectors/Models/CollectionEditModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Collectors.Models
+{
+    public class CollectionEditModel
+    {
+        [Required]
+        public int CollectionId { get; set; }
+
+        [Required]
+        [Display(Name = "Short description")]
+        [StringLength(1000)]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Image for collection")]
+        public IFormFile Image { get; set; }
+
+        public byte[] OldImage { get; set; }
+
+        public bool ShouldDeleteImage { get; set; }
+    }
+}
